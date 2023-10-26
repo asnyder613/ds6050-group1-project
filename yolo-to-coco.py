4063a42782f6ab510b5a1709654be187c5d48cf2
Dominic Scerbo <ybt7qf@virginia.edu>
2023-10-25 20:33:32 -0400

    fix

--- yolo-to-coco.py
+++ yolo-to-coco.py
@@ -1,21 +1,27 @@
 import os
 import argparse
 from pylabel import importer
+import yaml
 
 def main():
     parser = argparse.ArgumentParser(description="Import and analyze a dataset")
     parser.add_argument("--path_to_annotations", type=str, required=True, help="Path to annotations directory")
     parser.add_argument("--path_to_images", type=str, required=True, help="Path to images directory")
+    parser.add_argument("--path_to_yolo_yaml", type=str, required=True, help="Path to yolo yaml")
     parser.add_argument("--name", required=True, help="Name of the dataset")
     
     args = parser.parse_args()
     
-    current_script_path = os.path.dirname(os.path.abspath(__file__))
-    path_to_annotations = current_script_path + args.path_to_annotations
-    path_to_images = current_script_path + args.path_to_images
+    path_to_annotations = args.path_to_annotations
+    path_to_images = args.path_to_images
     name = args.name
-    
-    classes = ['person']
+
+    with open(args.path_to_yolo_yaml, 'r') as file:
+        content = file.read()
+
+    data = yaml.load(content, Loader=yaml.FullLoader)
+
+    classes = data['names']
     dataset = importer.ImportYoloV5(
         path=path_to_annotations,
         path_to_images=path_to_images,
